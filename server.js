1f8b6ec49e4e10f424594a3462617c387035e218
Yashub <rishubwork@gmail.com>
2018-10-20 18:28:42 -0500

    Installed Dependencies and Merged frontend-ui

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const items = require('./routes/api/items');
-
 const app = express();
 
 // Bodyparser Middleware
@@ -14,23 +12,10 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
-mongoose
-  .connect(db, {useNewUrlParser: true}) // Adding new mongo url parser
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
-
-// Use Routes
-app.use('/api/items', items);
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// mongoose
+//   .connect(db, {useNewUrlParser: true}) // Adding new mongo url parser
+//   .then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
