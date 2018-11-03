cbf673f90c2f2c796388a593be06f5322f4ec2b3
Yashub <rishubwork@gmail.com>
2018-11-03 17:48:38 -0500

    Moved sidebar to a seperate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,24 @@
 import React, { Component } from 'react';
 import {Container, ListGroup, ListGroupItem, ListGroupItemHeading, Button} from 'reactstrap';
-import Sidebar from 'react-sidebar';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import TaskList from './components/TaskList';
+import SideBar from './components/SideBar';
 
 
-const mql = window.matchMedia(`(min-width: 750px)`);
 
 
 class App extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			sidebarDocked: mql.matches,
-			sidebarOpen: false
-		};
-
-		this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
-		this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-	}
-
-	//REDUX
-
-	componentWillMount() {
-		mql.addListener(this.mediaQueryChanged);
-	}
 	
-	componentWillUnmount() {
-		mql.removeListener(this.mediaQueryChanged);
-	}
-	
-	onSetSidebarOpen(open) {
-		this.setState({ sidebarOpen: open });
-	}
-	
-	mediaQueryChanged() {
-		this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
-	}
 
 	render() {
 		return (
 			<div className="App">
-				<Container className="SideBar">
-					<Sidebar 
-					sidebar={
-						<ListGroup>
-							<ListGroupItemHeading><h1>Menu</h1></ListGroupItemHeading>
-							<Button className = "side-bar-btn">Calendar</Button>
-							<Button className = "side-bar-btn">Tasks</Button>
-							<ListGroupItemHeading>Tasks</ListGroupItemHeading>
-							<ListGroup>
-								<ListGroupItemHeading>Classes</ListGroupItemHeading>
-								<ListGroupItem>CS196</ListGroupItem>
-								<ListGroupItem>CS126</ListGroupItem>
-								<ListGroupItem>PHIL102</ListGroupItem>
-							</ListGroup>
-						</ListGroup>}
-					open={this.state.sidebarOpen}
-					docked={this.state.sidebarDocked}
-					onSetOpen={this.onSetSidebarOpen}
-					shadow={false}
-					>	
-					</Sidebar>
+				<Container  className="SideBar">
+				<SideBar />
 				</Container>
 				<Container className="Task">
 				<h2>Task Assignerç</h2>
@@ -91,4 +45,5 @@ Background color
 Category buttons
 Checkbox buttons left side 
 Checkbox buttons fill box
+Add button
 */
