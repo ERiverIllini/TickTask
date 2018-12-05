e0d3a96958c22935c5f169db53648989be539fcc
nacho <nks5@illinois.edu>
2018-12-05 10:42:28 -0600

    fire.js

---
+++ model/fire.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+src = "https://www.gstatic.com/firebasejs/5.5.9/firebase.js"
+var config = {
+  apiKey: "AIzaSyDMeXmgZcNDvAXtFBzVtW01_nfPPs7e_OI",
+  authDomain: "ticktask-1540076089537.firebaseapp.com",
+  databaseURL: "https://ticktask-1540076089537.firebaseio.com",
+  projectId: "ticktask-1540076089537",
+  storageBucket: "ticktask-1540076089537.appspot.com",
+  messagingSenderId: "592668257087"
+};
+var fire = firebase.initializeApp(config);
+var ref = new Firebase("https://ticktask-1540076089537.firebaseio.com/");
+ref.update({message:"Hello"});
