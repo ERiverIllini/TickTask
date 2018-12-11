2085a75393a7869bb36502c9cf1faf6f788165df
Snehal <snehals2@illinois.edu>
2018-12-11 15:36:19 -0600

    Got basics of login page working

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+(function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyD58g_fm3ltq9XOmSn1Vgs1HzTCRmOGvfk",
+        authDomain: "tick-task-db50f.firebaseapp.com",
+        databaseURL: "https://tick-task-db50f.firebaseio.com",
+        projectId: "tick-task-db50f",
+        storageBucket: "tick-task-db50f.appspot.com",
+        messagingSenderId: "455444650561"
+    };
+    firebase.initializeApp(config);
+
+    app_fireBase = firebase;
+})()
+
