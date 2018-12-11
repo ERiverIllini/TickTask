8be87de1a43bb8459724bf302082384fa82af3b7
Snehal <snehals2@illinois.edu>
2018-12-11 16:19:56 -0600

    Added logout button

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,27 @@ import TaskList from './components/TaskList';
 import TaskModal from './components/TaskModal';
 
 
+var mainApp = {};
 
+(function(){
+	//var firebase = app_fireBase
+var uid = null;
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+		  // User is signed in.
+		  uid = user.uid;
+		} else {
+			uid = null;
+			window.location.replace("login.html")
+		}
+	  });
+
+	  function logOut(){
+		  firebase.auth().signOut();
+	  }
+
+	  mainApp.logOut = logOut;
+})()
 
 class App extends Component {
 	render() {
